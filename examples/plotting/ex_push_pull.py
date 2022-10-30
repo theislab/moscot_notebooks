a612c5197f4b5ff52d923048656b1da27d344752
Dominik Klein <dominik.klein@helmholtz-munich.de>
2022-10-30 18:42:21 +0100

    add moscot plotting examples

---
+++ examples/plotting/ex_push_pull.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""
+Plotting pull or push distributions
+-----------------------------------
+"""
+
+###############################################################################
+# In this notebook, we will showcase how to use :meth:`moscot.plotting.push` and
+# its counterpart :meth:`moscot.plotting.pull`. 
+# These visualisation functions are only implemented for non-spatial problems. 
+# To see how pullback or pushforward cell distributions can be visualized for 
+# problems incorporating spatial information, please have a look at the tutorials.
+# Here, we use the HSPC dataset to demonstrate the usage of :meth:`moscot.plotting.push` and
+# :meth:`moscot.plotting.pull` with the :class:`moscot.problems.time.TemporalProblem`.
+# In this context, the pull-back distribution corresponds to the set of ancestor cells, while
+# the push-forward distribution corresponds to the set of descending cells.
+
+from moscot.datasets import hspc
+from moscot.problems.time import TemporalProblem
+import moscot.plotting as mpl
+
+adata = hspc()
+
+###############################################################################
+# First, we need to prepare and solve the problem. Here, we set the `threshold`
+# parameter to a relative high value to speed up convergence at the cost of 
+# lower quality.
+
+tp = TemporalProblem(adata).prepare(time_key="day").solve(epsilon=1e-2, threshold=1e-2)
+
+###############################################################################
+# As for all plotting functionalities in moscot, we first call the method of 
+# the problem class, which stores the results of the computation in the 
+# :class:`anndata.AnnData` instance. Let us assume we look for the descendants
+# of cells of time point 4 in time point 7. We can specify whether we want to 
+# return the result via the `return_data` parameter.
+tp.push(start=4, end=7, return_data=False)
+
+###############################################################################
+# We can now visualize the result. As we have multiple time points in the UMAP
+# embedding, it is best to visualize in one plot all the cells corresponding to
+# time point 4, and then the ones corresponding to the descending cells.
+# As the :class:`anndata.AnnData` instance contains UMAP embeddings for both
+# gene expression and ATAC, we need to define which one to use via `basis`.
+
+mpl.push(tp, time_points=[4], basis="umap_GEX")
+mpl.push(tp, time_points=[7], basis="umap_GEX")
+
+###############################################################################
+# By default, the result of the `push` method of a problem instance is saved
+# `anndata.AnnData.uns['moscot_results']['push']['push'] and overrides
+# this element every time the method is called. To prevent this, we can specify the parameter
+# `key_added`, which we will do to store the results of the following use case.
+
+###############################################################################
+# We can also visualize the descendants of only a subset of categories of an 
+# :attr:`anndata.AnnData.obs` column
+# by specifying the `data` and the `subset` parameter.
+new_key = "subset_push"
+tp.push(start=4, end=7, data="cell_type", subset="HSC", return_data=False, key_added=new_key)
+mpl.push(tp, time_points=[4], uns_key=new_key, basis="umap_GEX")
+mpl.push(tp, time_points=[7], uns_key=new_key, basis="umap_GEX")
---
+++ examples/plotting/ex_sankey.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""
+Plotting Sankey diagrams
+------------------------
+"""
+
+###############################################################################
+# In this notebook, we will showcase how to use :meth:`moscot.plotting.sankey`.
+# We use the HSPC dataset to demonstrate the usage.
+
+from moscot.datasets import hspc
+from moscot.problems.time import TemporalProblem
+import moscot.plotting as mpl
+
+adata = hspc()
+
+###############################################################################
+# First, we need to prepare and solve the problem. Here, we set the `threshold`
+# parameter to a relative high value to speed up convergence at the cost of 
+# lower quality.
+
+tp = TemporalProblem(adata).prepare(time_key="day").solve(epsilon=1e-2, threshold=1e-2)
+
+###############################################################################
+# As for all plotting functionalities in moscot, we first call the method of 
+# the problem class, which stores the results of the computation in the 
+# :class:`anndata.AnnData` instance. Let us assume we want to plot the sankey diagram
+# across all time points 2, 3, 4, and 7. Moreover, we want the Sankey diagram
+# to visualize flows between cell types. In general, we can visualize the flow defined 
+# by any column in :attr:`anndata.AnnData.obs` via the `source_groups` parameter and
+# the `target_groups` parameter, respectively. In this example, we are intereseted in 
+# descendants as opposed to ancestors, which is why we choose `forward` to be `True`.
+# The information required to plot the Sankey diagram is provided in transition matrices,
+# which we would obtain by `return_data` to `True`. Here, we are only interested in the
+# visualization.
+
+tp.sankey(source=2, target=7, source_groups="cell_type", target_groups="cell_type", forward=True, return_data=False)
+
+###############################################################################
+# Having called the `sankey` method of the problem instance, we now pass the result
+# to the :mod:`moscot.plotting` module. 
+# Therefore, we can either pass the :class:`anndata.AnnData` instance or the problem
+# instance. We can set the size of the figure via `dpi` and set a title via `title`. 
+
+mpl.sankey(tp, dpi=100, title="Cell type evolution over time")
+
+###############################################################################
+# By default, the result of the `sankey` method of a problem instance is saved
+# `anndata.AnnData.uns['moscot_results']['sankey']['sankey'] and overrides
+# this element every time the method is called. To prevent this, we can specify the parameter
+# `key_added`, which we will do to store the results of the following use case.
+
+###############################################################################
+# We can also visualize flows of only a subset of categories of an :attr:`anndata.AnnData.obs` column
+# by passing a dictionary for `source_groups` or `target_groups`. 
+new_key = "subset_sankey"
+tp.sankey(source=2, target=7, source_groups={"cell_type": ["HSC", "MasP", "MkP"]}, target_groups={"cell_type": ["HSC", "MasP", "MkP"]}, forward=True, return_data=False, key_added=new_key)
+mpl.sankey(tp, dpi=100, title="Cell type evolution over time", uns_key=new_key)
+
